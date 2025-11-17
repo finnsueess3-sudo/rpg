cf232762877f4946630d19284226dc6a32162dfa
finnsueess3-sudo <finn.sueess3@gmail.com>
2025-11-17 16:36:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,53 +5,44 @@ const io = require("socket.io")(http);
 
 app.use(express.static("public"));
 
-const MAP_SIZE = 2000;
+const MAP_SIZE = 3000;
 const PLAYERS = {};
 
 io.on("connection", socket => {
-  console.log("Player connected:", socket.id);
   PLAYERS[socket.id] = {
-    x: Math.random() * MAP_SIZE,
-    y: Math.random() * MAP_SIZE,
+    x: Math.random()*MAP_SIZE,
+    y: Math.random()*MAP_SIZE,
     class: null,
-    hp: 10,
+    hp: 5,
     mana: 5,
     level: 1,
     xp: 0,
     speed: 150,
     facing: "down",
-    abilities: { q: false, e: false, x: false },
-    status: {}
+    abilities: {q:false,e:false,x:false},
+    status:{},
   };
 
-  socket.emit("init", { id: socket.id, mapSize: MAP_SIZE, players: PLAYERS });
+  socket.emit("init",{id:socket.id,mapSize:MAP_SIZE,players:PLAYERS});
 
-  socket.on("selectClass", cls => {
-    const p = PLAYERS[socket.id];
-    p.class = cls;
-    switch(cls){
-      case "ninja": p.hp=3; p.speed=180; break;
-      case "mage": p.hp=5; p.mana=10; break;
-      case "warrior": p.hp=8; break;
-      case "archer": p.hp=5; break;
-      case "dragon": p.hp=10; p.mana=10; break;
-      case "rogue": p.hp=4; p.speed=170; break;
-    }
+  socket.on("selectClass",cls=>{
+    const p=PLAYERS[socket.id]; p.class=cls;
+    const baseStats={
+      ninja:{hp:3,speed:180},mage:{hp:5,mana:10},warrior:{hp:8},
+      archer:{hp:5},dragon:{hp:10,mana:10},rogue:{hp:4,speed:170},
+      paladin:{hp:9},berserker:{hp:7,speed:160},assassin:{hp:3,speed:190}
+    };
+    Object.assign(p,baseStats[cls]||{});
   });
 
-  socket.on("update", data => {
-    const p = PLAYERS[socket.id];
-    if(!p) return;
-    Object.assign(p, data);
+  socket.on("update",data=>{
+    const p=PLAYERS[socket.id]; if(!p) return;
+    Object.assign(p,data);
   });
 
-  socket.on("disconnect", () => {
-    delete PLAYERS[socket.id];
-  });
+  socket.on("disconnect",()=>delete PLAYERS[socket.id]);
 });
 
-setInterval(() => {
-  io.emit("playersUpdate", PLAYERS);
-}, 50);
+setInterval(()=>{ io.emit("playersUpdate",PLAYERS); },50);
 
-http.listen(3000, () => console.log("Server running on port 3000"));
+http.listen(3000,()=>console.log("Server running on port 3000"));
